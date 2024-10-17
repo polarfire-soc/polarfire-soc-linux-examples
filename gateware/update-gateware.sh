781a032ac2f3a5a7059afb50bda69e39634cba70
Jamie Gibbons <jamie.gibbons@microchip.com>
2024-10-17 09:07:26 +0100

    gateware: update gateware script
    Update flash_erase to only erase dtbos as erasing such a large amount
    of memory was overkill. A reboot, and therefore a firmware update is
    only desired if no error has occured in the validation stage. Add an
    additional conditional to only reboot if no error is reported. Use cat
    instead of dd command to set-up data as suggested by auto-update
    documentation. Update formatting and comments to match script in
    Buildroot - keeping a uniform copy for all build systems.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- gateware/update-gateware.sh
+++ gateware/update-gateware.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
 if [ $# -eq 0 ]; then
     echo "No gateware location provided. Checking default location."
     if [ ! -e /lib/firmware/mpfs_bitstream.spi ]; then
-         echo "No gateware file found."
-         exit 1
+        echo "No gateware file found."
+        exit 1
     fi
 else
     echo "Gateware location provided: $1"
@@ -18,17 +19,41 @@ else
         cp "$1"/mpfs_bitstream.spi /lib/firmware/mpfs_bitstream.spi
     fi
 fi
-1
-flash_erase /dev/mtd0 0 64
+
+
+# Trash existing device tree overlay in case the rest of the process fails:
+flash_erase /dev/mtd0 0 16
+
+# Get device tree overlay size
 dtbo_size=$(stat -c %s /lib/firmware/mpfs_dtbo.spi)
+
+# Write device tree overlay
 dd if=/lib/firmware/mpfs_dtbo.spi of=/dev/mtd0 bs=1 seek=$((0x400)) count="$dtbo_size" status=progress
+
+# Fake the presence of a golden image for now.
 dd if=/dev/zero of=/dev/mtd0 count=1 bs=4
-echo 0 > /sys/class/firmware/timeout
+
+# Initiate FPGA update.
 echo 1 > /sys/class/firmware/mpfs-auto-update/loading
-bitstream_size=$(stat -c %s /lib/firmware/mpfs_bitstream.spi)
-dd if=/lib/firmware/mpfs_bitstream.spi of=/sys/class/firmware/mpfs-auto-update/data bs=1 count="$bitstream_size" status=progress
+
+# Write the firmware image to the data sysfs file
+cat /lib/firmware/mpfs_bitstream.spi > /sys/class/firmware/mpfs-auto-update/data
+
+# Signal completion
 echo 0 > /sys/class/firmware/mpfs-auto-update/loading
+
 while [ "$(cat /sys/class/firmware/mpfs-auto-update/status)" != "idle" ]; do
-  sleep 1
+    # Do nothing, just keep checking
+    sleep 1
 done
-reboot
+
+# When the status is 'idle' and no error has occured, reboot the system for
+# the gateware update to take effect. FPGA reprogramming takes places between
+# Linux shut-down and HSS restarting the board.
+if [ "$(cat /sys/class/firmware/mpfs-auto-update/error)" = "" ]; then
+    echo "FPGA update ready. Rebooting."
+    reboot
+else
+    echo "FPGA update failed with status: $(cat /sys/class/firmware/mpfs-auto-update/error)"
+    exit 1
+fi
