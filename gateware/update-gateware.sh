5853e53eab77a774c99e1f42ebfc59381a07b4f5
Conor Dooley <conor.dooley@microchip.com>
2025-07-08 14:25:17 +0100

    gateware: support passing gateware/dtbo files explicitly
    On production kits, it may not be the case where a dtbo file is
    provided, fbe that not using the same infrastructure as the beagle
    to produce it automatically or something else. Permit passing the
    gateware and dtbo files explicitly, rather than just the directory
    containing files matching the default naming convention. This has the
    additional benefit of making keeping track of what is being programmed
    easier in an setting where there are multiple images involved.
    
    Copying files into /lib/firmware/ didn't seem like a required part of
    the process, so I removed it as part of this change.
    
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- gateware/update-gateware.sh
+++ gateware/update-gateware.sh
@@ -1,51 +1,79 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]; then
-    echo "No gateware location provided. Checking default location."
-    if [ ! -e /lib/firmware/mpfs_bitstream.spi ]; then
-        echo "No gateware file found."
-        exit 1
-    fi
-else
-    echo "Gateware location provided: $1"
-    if [ ! -e "$1"/mpfs_bitstream.spi ]; then
-        echo "No gateware file found."
-        exit 1
-    else
-        if [ ! -d /lib/firmware ]; then
-            mkdir /lib/firmware
-        fi
-        cp "$1"/mpfs_dtbo.spi /lib/firmware/mpfs_dtbo.spi
-        cp "$1"/mpfs_bitstream.spi /lib/firmware/mpfs_bitstream.spi
-    fi
+if [ $# -gt 2 ]; then
+    echo "Too many arguments provided."
 fi
 
+if [ $# -eq 0 ]; then
+    echo "No gateware location provided, checking default location (/lib/firmware/)"
+    gateware_location=/lib/firmware/mpfs_bitstream.spi
+    dtbo_location=/lib/firmware/mpfs_dtbo.spi
 
-# Trash existing device tree overlay in case the rest of the process fails:
-flash_erase /dev/mtd0 0 16
+    if [ ! -e $gateware_location ]; then
+        echo "No gateware file found."
+        exit 1
+    fi
 
-# Initiate FPGA update for dtbo
-echo 1 > /sys/class/firmware/mpfs-auto-update/loading
+    if [ ! -e $dtbo_location ]; then
+  echo "No dtbo file found."
+  exit 1
+    fi
+elif [ $# -eq 1 ]; then
+    if [ ! -e "$1" ]; then
+        echo "Gateware location invalid."
+        exit 1
+    fi
 
-# Write device tree overlay
-cat /lib/firmware/mpfs_dtbo.spi > /sys/class/firmware/mpfs-auto-update/data
+    if [ -d "$1" ]; then
+        gateware_location="$1"/mpfs_dtbo.spi
+        dtbo_location="$1"/mpfs_bitstream.spi
+    else
+        gateware_location=$1
+    fi
+elif [ $# -eq 2 ]; then
+    if [ ! -e "$1" ]; then
+        echo "No gateware file found."
+        exit 1
+    fi
+    if [ ! -e "$2" ]; then
+        echo "No dtbo file found."
+        exit 1
+    fi
+    gateware_location=$1
+    dtbo_location=$2
+fi
 
-# Signal completion for dtbo load
-echo 0 > /sys/class/firmware/mpfs-auto-update/loading
+echo "Using gateware file $gateware_location"
 
-while [ "$(cat /sys/class/firmware/mpfs-auto-update/status)" != "idle" ]; do
-    # Do nothing, just keep checking
-    sleep 1
-done
+if [ -n "$dtbo_location" ] && [ -e "$dtbo_location" ]; then
+    echo "Using dtbo file $dtbo_location"
+
+    # Trash existing device tree overlay in case the rest of the process fails:
+    flash_erase /dev/mtd0 0 16
+
+    # Initiate FPGA update for dtbo
+    echo 1 > /sys/class/firmware/mpfs-auto-update/loading
+
+    # Write device tree overlay
+    cat "$dtbo_location" > /sys/class/firmware/mpfs-auto-update/data
+
+    # Signal completion for dtbo load
+    echo 0 > /sys/class/firmware/mpfs-auto-update/loading
+
+    while [ "$(cat /sys/class/firmware/mpfs-auto-update/status)" != "idle" ]; do
+        # Do nothing, just keep checking
+        sleep 1
+    done
+fi
 
 # Fake the presence of a golden image for now.
-dd if=/dev/zero of=/dev/mtd0 count=1 bs=4
+dd if=/dev/zero of=/dev/mtd0 count=1 bs=4 2>/dev/null
 
 # Initiate FPGA update for bitstream
 echo 1 > /sys/class/firmware/mpfs-auto-update/loading
 
 # Write the firmware image to the data sysfs file
-cat /lib/firmware/mpfs_bitstream.spi > /sys/class/firmware/mpfs-auto-update/data
+cat "$gateware_location" > /sys/class/firmware/mpfs-auto-update/data
 
 # Signal completion for bitstream load
 echo 0 > /sys/class/firmware/mpfs-auto-update/loading
