d489f42518392843b18dc60658b666081a2d156b
Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>
2026-01-22 12:00:53 +0000

    gpio: update Linux example to libgpiod v2 API
    The PolarFire SoC GPIO Linux example used the deprecated libgpiod v1 API.
    Recent Buildroot and Yocto releases default to libgpiod v2, causing
    build failures.
    
    Update the example to use the libgpiod v2 API while preserving existing
    functionality.
    
    Signed-off-by: Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>

--- gpio/gpiod-event.c
+++ gpio/gpiod-event.c
@@ -1,91 +1,150 @@
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: MIT
 /*
- * GPIO event capture example -- Detects edge events and keeps count
+ * GPIO example for the Microchip PolarFire SoC.
  *
+ * Copyright (c) 2026 Microchip Technology Inc.
  */
 
 #include <gpiod.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-#ifndef	CONSUMER
-#define	CONSUMER	"Consumer"
+#ifndef CONSUMER
+#define CONSUMER "Consumer"
 #endif
 
 int main(int argc, char **argv)
 {
-	char *chipname = "gpiochip0";
-	unsigned int line_num = 30;	// GPIO Pin <sw2> 
-	struct timespec ts = { 1, 0 };
-	struct gpiod_line_event event;
-	struct gpiod_chip *chip;
-	struct gpiod_line *line;
-	int i, ret;
+	int ret, count_event = 0, previous_value = 1;
+	const char *chipname = "gpiochip0";
+	unsigned int line_num;
+	char chip_path[64];
+	const int64_t timeout_ns = 1000000000LL; /* 1 second */
 
-	if(argc != 2) {
-		perror("Incorrect parameters passed: gpiod-event <gpio pin> \n");
-		ret = -1;
-		goto end;
+	struct gpiod_chip *chip = NULL;
+	struct gpiod_line_settings *settings = NULL;
+	struct gpiod_line_config *line_cfg = NULL;
+	struct gpiod_request_config *req_cfg = NULL;
+	struct gpiod_line_request *request = NULL;
+	struct gpiod_edge_event_buffer *evbuf = NULL;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <gpio pin>\n", argv[0]);
+		return -1;
 	}
 
-	line_num = atoi(argv[1]);
-	if(line_num > 31){
-		perror("Max <gpio pin> 31\n");
-		ret = -1;
-		goto end;
+	line_num = (unsigned int)atoi(argv[1]);
+	if (line_num > 31) {
+		fprintf(stderr, "Max <gpio pin> 31\n");
+		return -1;
 	}
 
-	chip = gpiod_chip_open_by_name(chipname);
+	snprintf(chip_path, sizeof(chip_path), "/dev/%s", chipname);
+	chip = gpiod_chip_open(chip_path);
 	if (!chip) {
-		perror("Open chip failed\n");
+		perror("Failed to open GPIO chip\n");
 		ret = -1;
 		goto end;
 	}
 
-	line = gpiod_chip_get_line(chip, line_num);
-	if (!line) {
-		perror("Get line failed\n");
+ 	settings = gpiod_line_settings_new();
+	if (!settings) {
+		perror("Failed to allocate GPIO line settings\n");
 		ret = -1;
 		goto close_chip;
 	}
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+	gpiod_line_settings_set_edge_detection(settings, GPIOD_LINE_EDGE_RISING);
 
-	ret = gpiod_line_request_rising_edge_events(line, CONSUMER);
-	if (ret < 0) {
-		perror("Request event notification failed\n");
+	line_cfg = gpiod_line_config_new();
+	if (!line_cfg) {
+		perror("Failed to allocate GPIO line configuration\n");
 		ret = -1;
-		goto release_line;
+		goto free_settings;
 	}
 
-	/* Run for 20 1 second windows */
-	i = 0;
-	int count_event = 0;
-	printf("Application will run for 20 1 second event windows, press ctrl+c to exit early.\n");
-	while (i++ < 20) {
-		ret = gpiod_line_event_wait(line, &ts);
+	ret = gpiod_line_config_add_line_settings(line_cfg, &line_num, 1, settings);
+	if (ret < 0) {
+		perror("Failed to apply line settings to requested GPIO line\n");
+		goto free_line_cfg;
+	}
+
+	req_cfg = gpiod_request_config_new();
+	if (!req_cfg) {
+		perror("Failed to allocate GPIO request configuration\n");
+		ret = -1;
+		goto free_line_cfg;
+	}
+	gpiod_request_config_set_consumer(req_cfg, CONSUMER);
+
+	request = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+	if (!request) {
+		perror("Failed to request GPIO line\n");
+		ret = -1;
+		goto free_req_cfg;
+	}
+
+	evbuf = gpiod_edge_event_buffer_new(16);
+	if (!evbuf) {
+		perror("Failed to allocate edge event buffer\n");
+		ret = -1;
+		goto release_request;
+	}
+
+	printf("Monitoring GPIO events for 20 seconds (Ctrl+C to terminate)\n\n");
+
+	for (int i = 0; i < 20; i++) {
+		ret = gpiod_line_request_wait_edge_events(request, timeout_ns);
 		if (ret < 0) {
-			perror("gpiod_line_event_wait Failed\n");
+			perror("Failed while waiting for GPIO edge event\n");
 			ret = -1;
-			goto release_line;
+			goto free_evbuf;
 		} else if (ret == 0) {
 			printf("No event notification received on line  #%u\n", line_num);
 			continue;
 		}
 
-		ret = gpiod_line_event_read(line, &event);
-		count_event++;
-		printf("Got event notification on line #%u %d times\n", line_num, count_event);
+		ret = gpiod_line_request_read_edge_events(request, evbuf, 1);
 		if (ret < 0) {
-			perror("gpiod_line_event_read failed\n");
+			perror("Failed to read GPIO edge event\n");
 			ret = -1;
-			goto release_line;
+			goto free_evbuf;
 		}
-		sleep(1);
+
+	if (ret > 0) {
+		for (int j = 0; j < ret; j++) {
+			int val = gpiod_line_request_get_value(request, line_num);
+
+			if (val < 0)
+				continue;
+
+			if (val == 0 && previous_value != 0) {
+				count_event += ret;
+				printf("Got event notification on line #%u %d times \n",
+						line_num, count_event);
+			}
+
+			previous_value = val;
+		}
+	}
+
+	sleep(1);
 	}
 
 	ret = 0;
 
-release_line:
-	gpiod_line_release(line);
+free_evbuf:
+	gpiod_edge_event_buffer_free(evbuf);
+release_request:
+	gpiod_line_request_release(request);
+free_req_cfg:
+	gpiod_request_config_free(req_cfg);
+free_line_cfg:
+	gpiod_line_config_free(line_cfg);
+free_settings:
+	gpiod_line_settings_free(settings);
 close_chip:
 	gpiod_chip_close(chip);
 end:
--- gpio/gpiod-test.c
+++ gpio/gpiod-test.c
@@ -1,78 +1,122 @@
 // SPDX-License-Identifier: MIT
 /*
- * GPIOD example -- read SW2 switch state
+ * GPIO example for the Microchip PolarFire SoC.
  *
- * Copyright (c) 2021 Microchip Inc.
+ * Copyright (c) 2026 Microchip Technology Inc. All rights reserved.
  */
 
-#include <errno.h>
-#include <fcntl.h>
-#include <gpiod.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <gpiod.h>
 
-#define SW2_GPIO30	30
+static int read_gpio_state(struct gpiod_chip *chip, unsigned int line_num)
+{
+	int counter = 0, value = -1, previous_value = -1;
+	struct gpiod_line_settings *settings = NULL;
+	struct gpiod_line_config *line_cfg = NULL;
+	struct gpiod_request_config *req_cfg = NULL;
+	struct gpiod_line_request *gpio_req = NULL;
 
-int main()
+	settings = gpiod_line_settings_new();
+	if (!settings) {
+		perror("Failed to initialize GPIO line settings\n");
+		return -1;
+	}
+
+	gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT);
+
+	line_cfg = gpiod_line_config_new();
+	if (!line_cfg) {
+		perror("Failed to allocate GPIO line configuration\n");
+		goto free_settings;
+	}
+
+	gpiod_line_config_add_line_settings(line_cfg, &line_num, 1, settings);
+
+	req_cfg = gpiod_request_config_new();
+	if (!req_cfg) {
+		perror("Failed to set up GPIO request configuration\n");
+		goto free_line_cfg;
+	}
+
+	gpiod_request_config_set_consumer(req_cfg, "gpio-input");
+
+	gpio_req = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+	if (!gpio_req) {
+		printf("\n\tFailed to request GPIO# %d\n", line_num);
+		goto free_req_cfg;
+	}
+
+	printf("\nReading GPIO state for 20 seconds\n");
+	counter = 20;
+
+	while (counter--) {
+		sleep(1);
+
+		value = gpiod_line_request_get_value(gpio_req, line_num);
+		if (value < 0) {
+			printf("\n\tError reading GPIO# %d\n", line_num);
+			continue;
+		}
+
+		if (value != previous_value)
+			printf("\n\tGPIO state: %d\n", value);
+
+		previous_value = value;
+	}
+
+	gpiod_line_request_release(gpio_req);
+
+free_req_cfg:
+	gpiod_request_config_free(req_cfg);
+free_line_cfg:
+	gpiod_line_config_free(line_cfg);
+free_settings:
+	gpiod_line_settings_free(settings);
+
+	return 0;
+}
+
+int main(int argc, char **argv)
 {
 	char runcmd;
-	char *chipname = "/dev/gpiochip0";
+	static const char *const chip_path = "/dev/gpiochip0";
 	struct gpiod_chip *chip;
-	int counter = 0;
+	unsigned int line_num;
 
-	chip = gpiod_chip_open(chipname);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <gpio pin>\n", argv[0]);
+		return -1;
+	}
+
+	line_num = (unsigned int)atoi(argv[1]);
+	if (line_num > 31) {
+		fprintf(stderr, "Max <gpio pin> 31\n");
+		return -1;
+	}
+
+	chip = gpiod_chip_open(chip_path);
 	if (!chip) {
-		perror("Open chip failed\n");
-		goto end;
+		printf("Failed to open GPIO chip\n");
+		return EXIT_FAILURE;
 	}
 
 	while (1) {
-		printf("\n\t# Choose one of the following options:");
-		printf("\n\tEnter '1' to read the state of SW2 connected to GPIO30");
-		printf("\n\tEnter 'any key' to exit: ");
+		printf("\nPress 1 to read GPIO state\n"
+		       "Press any other key to exit\n");
 
 		scanf("%c%*c", &runcmd);
 
 		if (runcmd == '1') {
-			struct gpiod_line *sw2_30;
-			int value = -1;
-			int previous_value = -1;
-
-			sw2_30 = gpiod_chip_get_line(chip, SW2_GPIO30);
-			gpiod_line_request_input(sw2_30, "gpio30-sw2");
-
-			printf("\n Loop for 20 seconds reading the state of SW2, "
-			       "connected as an input to GPIO30\n");
-
-			counter = 20;
-			while (counter--) {
-				sleep(1);
-
-				value = gpiod_line_get_value(sw2_30);
-				if (value == -1) {
-					printf("\n\tError reading GPIO value: GPIO# %d\n",
-					       SW2_GPIO30);
-				} else {
-					if (value != previous_value)
-						printf("\n\tSW2 switch value is now: %d\n",
-						       value);
-					previous_value = value;
-				}
-			}
-
-			gpiod_line_release(sw2_30);
+			read_gpio_state(chip, line_num);
 		} else {
-			printf("\n\tExiting GPIO example program...\n");
-			goto close_chip;
+			printf("\n\tExiting GPIO example\n");
+			break;
 		}
 	}
 
-close_chip:
 	gpiod_chip_close(chip);
-end:
 	return 0;
 }
